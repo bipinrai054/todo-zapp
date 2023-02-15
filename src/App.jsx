1d21c19893d873a24b86809e970d751a575e3427
bipnrai054 <bipinrai054@gmail.com>
2023-02-15 14:09:05 +0545

    layout done

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Button } from 'primereact/button';
+import './App.css';
+import 'primeicons/primeicons.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const addHandler = () => {
+    setTodos([...todos, todo]);
+    setTodo('');
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='App'>
+      <div className='  flex gap-3  '>
+        <InputText
+          placeholder='Items . . .'
+          className='p-2'
+          value={todo}
+          onChange={(e) => setTodo(e.target.value)}
+        />
+        <Button
+          className='bg-green-500 p-2 hover:bg-green-600  text-2xl'
+          onClick={addHandler}
+        >
+          +
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='mt-3'>
+        {todos.map((t, i) => {
+          return (
+            <div className='p-2 mt-2 bg-gray-800 flex justify-between' key={i}>
+              <p>{t}</p>
+              <div className='flex gap-2'>
+                <i className='pi pi-pencil text-blue-500 mt-1 hover:text-blue-600'></i>
+                <i className='pi pi-trash text-red-500 mt-1 hover:text-red-600'></i>
+              </div>
+            </div>
+          );
+        })}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
